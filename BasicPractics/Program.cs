7cb6a70459329845635619fb4f4431073df4cb57
asikurr <asikurrahman7@gmail.com>
2023-01-24 17:13:57 +0600

    Added probem solving project.

--- BasicPractics/Program.cs
+++ BasicPractics/Program.cs
@@ -5,4 +5,30 @@ using BasicPractics;
 
 //new Swap().swapwithtemp();
 
-new Swap().swapwith();
+//new Swap().swapwith();
+/*
+int number = int.Parse(Console.ReadLine());
+int[] num = new int[number];
+int i;
+for (i = 0;i < number; i++)
+{
+    Console.WriteLine("Give "+i+"th position value");
+    int nums = int.Parse(Console.ReadLine());
+    num[i] = nums;
+    
+}*/
+//Console.WriteLine(num);
+int nums = int.Parse(Console.ReadLine());
+int nums1 = int.Parse(Console.ReadLine());
+
+for (int m = 0; m< nums; m++)
+{
+    for (int n = nums1; n>m; n--)
+    {
+        Console.Write(nums);
+    }
+    Console.WriteLine();
+    
+}
+
+
---
+++ ProblemSolving/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num = int.Parse(Console.ReadLine());
+for (int i = 1; i <= num; i++)
+{
+    string n = Console.ReadLine().ToLower();
+    if (n.Length >= 1 && n.Length <= 100)
+    {
+        if (n.Length >= 11)
+        {
+            string fistIndex = n.Substring(0, 1);
+            int countVal = n.Length - 2;
+            string lastIndex = n.Substring(n.Length - 1);
+            Console.WriteLine("{0}{1}{2}", fistIndex, countVal, lastIndex);
+        }
+        else
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
