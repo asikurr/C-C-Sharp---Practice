c3559f8a9b73cb1cc8ce99ed1c48d4861d0c964e
asikurr <asikurrahman7@gmail.com>
2023-02-26 16:28:59 +0600

    problem solving practice

---
+++ matrixTransit/Program.cs
@@ -0,0 +1,44 @@
+﻿int x, y, r, c;
+
+int[,] simple = new int[50,50];
+int[,] tran = new int[50, 50];
+Console.WriteLine("Please give Row Number.");
+r = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Please give Coloum Number.");
+c = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Please give Row*Col Value.");
+for (int i = 0;i < r; i++)
+{
+	for (int j = 0; j < c; j++)
+	{
+        Console.Write($"[{i}]x[{j}] : " );
+        simple[i, j] = int.Parse(Console.ReadLine()!);
+    }
+}
+Console.WriteLine("the simple matrix is ");
+for (int i = 0; i < r; i++)
+{
+    for (int j = 0; j < c; j++)
+    {
+        Console.Write($"\t{simple[i, j]}");
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0;i<r; i++)
+{
+    for (int j = 0; j < c; j++)
+    {
+        tran[j, i] = simple[i, j];
+    }
+}
+
+Console.WriteLine("The transfer row to coloum matrix is ");
+for (int i = 0; i < r; i++)
+{
+    for (int j = 0; j < c; j++)
+    {
+        Console.Write($"\t{tran[i, j]}");
+    }
+    Console.WriteLine();
+}
